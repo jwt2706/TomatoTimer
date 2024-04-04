eddab88e92b1ab515b9fa62ae8348e650e72b9b7
jwt2706 <jwt2home@gmail.com>
2024-04-03 22:16:50 -0400

    bigger ui + arrows

--- applet.js
+++ applet.js
@@ -2,11 +2,11 @@ const Applet = imports.ui.applet;
 const PopupMenu = imports.ui.popupMenu;
 const St = imports.gi.St;
 
-function YourApplet(orientation, panelHeight, instanceId) {
+function TomatoTimer(orientation, panelHeight, instanceId) {
   this._init(orientation, panelHeight, instanceId);
 }
 
-YourApplet.prototype = {
+TomatoTimer.prototype = {
   __proto__: Applet.TextApplet.prototype,
 
   _init: function (orientation, panelHeight, instanceId) {
@@ -19,27 +19,75 @@ YourApplet.prototype = {
 
     this.set_applet_label("No time set");
 
-    // popup menu
     this.menu = new Applet.AppletPopupMenu(this, orientation);
     this.menuManager = new PopupMenu.PopupMenuManager(this);
     this.menuManager.addMenu(this.menu);
 
-    // create a text entry field for the timer duration
-    this.timerEntry = new St.Entry({ text: "Enter timer duration in seconds" });
-    this.menu.addMenuItem(new PopupMenu.PopupBaseMenuItem({ activate: false }));
-    this.menu.box.add(this.timerEntry);
+    // create three text entry fields for the timer duration
+    this.hoursEntry = this._createEntry();
+    this.minutesEntry = this._createEntry();
+    this.secondsEntry = this._createEntry();
 
-    // add an event listener for the 'activate' event
-    this.menu.connect("open-state-changed", (menu, isOpen) => {
-      if (isOpen) {
-        const timerDuration = parseInt(this.timerEntry.get_text()) * 1000;
-        setTimeout(() => {
-          global.log("Timer finished!");
-          this.set_applet_label("No time set");
-        }, timerDuration);
-        this.set_applet_label(this.timerEntry.get_text() + " seconds");
-      }
+    // create a box layout and add the entries and ":" labels to it
+    this.timerBox = new St.BoxLayout();
+    this.timerBox.add(this._createEntryBox(this.hoursEntry));
+    this.timerBox.add(new St.Label({ text: ":" }));
+    this.timerBox.add(this._createEntryBox(this.minutesEntry));
+    this.timerBox.add(new St.Label({ text: ":" }));
+    this.timerBox.add(this._createEntryBox(this.secondsEntry));
+
+    this.menu.addMenuItem(new PopupMenu.PopupBaseMenuItem({ activate: false }));
+    this.menu.box.add(this.timerBox);
+  },
+
+  _createEntry: function () {
+    const entry = new St.Entry({ text: "00" });
+    entry.set_style("font-size: 40px");
+    entry.clutter_text.set_editable(true);
+    entry.clutter_text.connect("activate", () => this._startTimer());
+    return entry;
+  },
+
+  _createEntryBox: function (entry) {
+    const box = new St.BoxLayout({ vertical: true });
+
+    const upButton = new St.Button({ label: "▲" });
+    upButton.connect("clicked", () => {
+      const value = Math.min(99, parseInt(entry.get_text()) + 1);
+      entry.set_text(value.toString().padStart(2, "0"));
     });
+
+    const downButton = new St.Button({ label: "▼" });
+    downButton.connect("clicked", () => {
+      const value = Math.max(0, parseInt(entry.get_text()) - 1);
+      entry.set_text(value.toString().padStart(2, "0"));
+    });
+
+    box.add(upButton);
+    box.add(entry);
+    box.add(downButton);
+
+    return box;
+  },
+
+  _startTimer: function () {
+    const timerDuration =
+      this._parseTime(
+        this.hoursEntry.get_text(),
+        this.minutesEntry.get_text(),
+        this.secondsEntry.get_text()
+      ) * 1000;
+    setTimeout(() => {
+      global.log("Timer finished!");
+      this.set_applet_label("No time set");
+    }, timerDuration);
+    this.set_applet_label(
+      `${this.hoursEntry.get_text()}:${this.minutesEntry.get_text()}:${this.secondsEntry.get_text()}`
+    );
+  },
+
+  _parseTime: function (hoursString, minutesString, secondsString) {
+    return +hoursString * 60 * 60 + +minutesString * 60 + +secondsString;
   },
 
   on_applet_clicked: function () {
@@ -48,5 +96,5 @@ YourApplet.prototype = {
 };
 
 function main(metadata, orientation, panelHeight, instanceId) {
-  return new YourApplet(orientation, panelHeight, instanceId);
+  return new TomatoTimer(orientation, panelHeight, instanceId);
 }
